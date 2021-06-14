17a84d70a011a2eaec16b976ac85db298eee9353
Nathan Page <33336363+ndpage@users.noreply.github.com>
2021-06-13 21:20:43 -0400

    Completed graded assignments for week 3 (Strings)

--- Week3/files.py
+++ Week3/files.py
@@ -1,2 +1,22 @@
 # Open files
 
+
+
+string = "abc"
+integer = 1
+floating = 1.11
+boolean = True
+
+cluster = (string, integer, floating, boolean)
+
+print(type(cluster))
+
+for each in cluster:
+  print(each)
+
+# for item in cluster:
+#   print(type(item))
+#   methods = dir(item)
+#   for method in methods:
+#     print(method)
+  
